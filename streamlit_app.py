713b56ed631422f7efa2dd356924c168576fe29e
nachomartinorovio-ux <nachomartinorovio@gmail.com>
2026-02-24 23:18:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
-streamlit
-google-generativeai
-st-gsheets-connection
+import streamlit as st
+import google.generativeai as genai
+
+# Leemos la llave de la caja fuerte (Secrets)
+api_key = st.secrets["GEMINI_API_KEY"]
+genai.configure(api_key=api_key)
+
+st.title("🎨 Sonarte Core")
+st.write("¡Chat inteligente conectado!")
